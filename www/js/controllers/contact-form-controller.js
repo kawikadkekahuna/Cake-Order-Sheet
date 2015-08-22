74d714203d218b292fdebaff7df4f30dca66f8ff
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-08-22 18:08:15 +0000

    messing around with fancy-select

--- www/js/controllers/contact-form-controller.js
+++ www/js/controllers/contact-form-controller.js
@@ -1,6 +1,6 @@
 angular.module('starter')
 
-.controller('ContactFormController', function($scope) {
+.controller('ContactFormController', function($scope,$state) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -12,6 +12,19 @@ angular.module('starter')
 
   $scope.init = function(){
 
+  }
+
+  $scope.next = function(contactData){
+    console.log('first',contactData.first_name);
+    console.log('last',contactData.last_name);
+    console.log('phone',contactData.phone_number);
+    var contactInfo =  {
+      first_name: contactData.first_name,
+      last_name: contactData.last_name,
+      phone_number: contactData.phone_number
+    };
+
+    $state.go('nav.order-form',contactInfo);
   }
 
 });
--- www/js/controllers/order-form-controller.js
+++ www/js/controllers/order-form-controller.js
@@ -1,6 +1,6 @@
 angular.module('starter')
 
-.controller('OrderFormController', function($scope) {
+.controller('OrderFormController', function($scope,$stateParams) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -8,7 +8,11 @@ angular.module('starter')
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
-  //
+  $scope.contactData = {
+    first_name:$stateParams.first_name,
+    last_name:$stateParams.last_name,
+    phone_number:$stateParams.phone_number
+  };
 
   $scope.init = function(){
 
